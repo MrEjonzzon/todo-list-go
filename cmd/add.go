3632ac13e5ca386a88194ca86a8fe90a2696d2f6
MrEjonzzon <emil@emiljo.com>
2026-07-18 20:51:05 +0200

    add more fields to add.go

--- cmd/add.go
+++ cmd/add.go
@@ -4,13 +4,38 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/spf13/cobra"
 )
 
+// Source - https://stackoverflow.com/a/64632817
+// Posted by colm.anseo
+// Retrieved 2026-07-18, License - CC BY-SA 4.0
+
+type autoInc struct {
+	sync.Mutex // ensures autoInc is goroutine-safe
+	id         int
+}
+
+func (a *autoInc) ID() (id int) {
+	a.Lock()
+	defer a.Unlock()
+
+	id = a.id
+	a.id++
+	return
+}
+
+var ai autoInc // global instance
+
 type Task struct {
+	Id          int    `json:"id"`
 	Description string `json:"description"`
 	Done        bool   `json:"done"`
+	CreatedAt   string `json:"createdAt"`
+	UpdatedAt   string `json:"updatedAt"`
 }
 
 var addCmd = &cobra.Command{
@@ -21,7 +46,13 @@ var addCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		description := args[0]
 		tasks := loadTasks()
-		tasks = append(tasks, Task{Description: description, Done: false})
+		tasks = append(tasks, Task{
+			Id:          ai.ID(),
+			Description: description,
+			Done:        false,
+			CreatedAt:   time.Now().Format("2006-01-02T15:04:05Z"),
+			UpdatedAt:   time.Now().Format("2006-01-02T15:04:05Z"),
+		})
 		saveTasks(tasks)
 		fmt.Printf("Task added: %s\n", description)
 	},
